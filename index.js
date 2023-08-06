44d0d6943e04c61286b67843af35a8370cb62850
Kabin119245 <85396529+Kabin119245@users.noreply.github.com>
2023-08-06 22:49:14 +0545

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const logo = document.getElementById("logo");
+    const homePageURL = "./index.html";
+
+    logo.addEventListener("click", function () {
+      window.location.href = homePageURL;
+    });
+  });
+
+
+
 function toggleMenu() {
   const navbar = document.querySelector(".navbar");
   navbar.classList.toggle("active");
 }
+
+var counter = 1;
+setInterval(function(){
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if (counter>4) {
+    counter = 1;
+  }
+},5000);
